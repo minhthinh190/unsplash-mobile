728cd7714a8ca28c0bb192427617a0c25dc508bb
justmb2 <17021298@vnu.edu.vn>
2021-03-24 22:55:30 +0700

    create photo model

---
+++ lib/model/photo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Photo {
+  final String id;
+  final String url;
+  
+  Photo({@required this.id, @required this.url});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      url: json['urls']['small'],
+    );
+  }
+}
