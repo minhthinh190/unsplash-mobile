3e1f489f681a043a7c03292d0b8d37ccc5e59880
minhthinh190 <dadzaikitano@gmail.com>
2021-03-24 15:18:16 +0700

    create home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unsplash_mobile/screens/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +7,8 @@ class MyApp extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Unsplash'),
-        ),
-        body: const Center(
-          child: const Text('Hello World'),
-        ),
-      ),
+      title: 'Unsplash',
+      home: Home(),
     );
   }
 }
