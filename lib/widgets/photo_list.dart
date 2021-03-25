3e041d4ad43a3f681d7049d08385566f9b3ebd82
minhthinh190 <dadzaikitano@gmail.com>
2021-03-25 10:24:58 +0700

    create photo list widget

---
+++ lib/widgets/photo_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:unsplash_mobile/model/photo.dart';
+
+Widget photoList({List<Photo> photos, context}) {
+  return Container(
+    margin: EdgeInsets.only(top: 20, right: 0, left: 0, bottom: 20),
+    padding: EdgeInsets.symmetric(horizontal: 30),
+
+    child: GridView.count(
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      physics: ClampingScrollPhysics(),
+      childAspectRatio: 0.9,
+      mainAxisSpacing: 20.0,
+      crossAxisSpacing: 15.0,
+
+      children: photos.map((photo) {
+        return GridTile(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(photo.src.portrait, fit: BoxFit.cover,),
+          ),
+        );
+      }).toList(),
+    ),
+  );
+}
