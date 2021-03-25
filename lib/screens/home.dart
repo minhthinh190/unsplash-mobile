20bed660918b89a84acb8cf502c2ff3c2dd4b3e4
minhthinh190 <dadzaikitano@gmail.com>
2021-03-25 10:29:04 +0700

    update home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,56 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:unsplash_mobile/widgets/appbar.dart';
 import 'package:unsplash_mobile/widgets/bottom_bar.dart';
 
-class Home extends StatelessWidget {
+import 'package:unsplash_mobile/data/api.dart';
+import 'package:unsplash_mobile/model/photo.dart';
+import 'package:unsplash_mobile/widgets/photo_list.dart';
+
+class Home extends StatefulWidget {
+  @override 
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Photo> photos = new List();
+
+  getNewPhotos() async {
+    final response = await http.get(
+      newPhotos,
+      headers: {"Authorization": apiKey},
+    );
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      Photo photo = new Photo();
+      photo = Photo.fromMap(element);
+      photos.add(photo);
+    });
+
+    setState(() {});
+  }
+
+  @override 
+  void initState() {
+    super.initState();
+    getNewPhotos();
+  }
+
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      body: const Center(
-        child: const Text('Home Screen'),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              photoList(photos: photos, context: context),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomBar(selectedIndex: 0),
     );
