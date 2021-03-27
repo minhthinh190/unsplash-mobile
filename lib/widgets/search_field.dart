82d0dd3df744720fb93f7efd0afd69a82a10228b
rossoneriNN <17021312@vnu.edu.vn>
2021-03-27 17:14:04 +0700

    update search field widget

--- lib/widgets/search_field.dart
+++ lib/widgets/search_field.dart
@@ -13,9 +13,9 @@ class SearchField extends StatelessWidget {
         color: Color(0xffeceff1),
         borderRadius: BorderRadius.circular(30),
       ),
-    
+
       padding: EdgeInsets.symmetric(horizontal: 24),
-      margin: EdgeInsets.only(top: 20, right: 30, left: 30, bottom: 10),
+      margin: EdgeInsets.only(top: 20, right: 30, left: 30, bottom: 20),
 
       child: Row(
         children: <Widget>[
