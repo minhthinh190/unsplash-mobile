0ba6575fa9bff55eaae6ddb88b812a5c48aabdd4
minhthinh190 <dadzaikitano@gmail.com>
2021-03-27 17:35:28 +0700

    update home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:unsplash_mobile/widgets/appbar.dart';
 import 'package:unsplash_mobile/widgets/bottom_bar.dart';
+import 'package:unsplash_mobile/widgets/category_list.dart';
 
 import 'package:unsplash_mobile/data/api.dart';
 import 'package:unsplash_mobile/model/photo.dart';
@@ -16,13 +17,32 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Photo> photos = new List();
-  
+  List<String> items = ['Wallpapers', 'Nature', 'People', 'Architecture', 'Current Event', 'Fashion', 'Travel']; 
+ 
   getNewPhotos() async {
     final response = await http.get(
       "https://api.pexels.com/v1/curated?per_page=16",
       headers: {"Authorization": apiKey},
     );
 
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      Photo photo = new Photo();
+      photo = Photo.fromMap(element);
+      photos.add(photo);
+    });
+
+    setState(() {});
+  }
+
+  getSearchedPhotos(String query) async {
+    final response = await http.get(
+      "https://api.pexels.com/v1/search?query=$query&per_page=16",
+      headers: {"Authorization": apiKey},
+    );
+
+    photos.clear();
+    
     Map<String, dynamic> jsonData = jsonDecode(response.body);
     jsonData["photos"].forEach((element) {
       Photo photo = new Photo();
@@ -47,6 +67,7 @@ class _HomeState extends State<Home> {
         child: Container(
           child: Column(
             children: <Widget>[
+              categoryList(items: items, callback: getSearchedPhotos),
               photoList(photos: photos, context: context),
             ],
           ),
