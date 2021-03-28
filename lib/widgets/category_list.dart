94d32e7b88966dc2a091ae6ab45f596276936843
minhthinh190 <dadzaikitano@gmail.com>
2021-03-28 17:02:47 +0700

    update category list

--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -1,43 +1,103 @@
 import 'package:flutter/material.dart';
 
-Widget categoryList({List<String> items, Function(String) callback, context}) {
-  return Container(
-    margin: EdgeInsets.symmetric(vertical: 10),
-    padding: EdgeInsets.symmetric(horizontal: 30),
-    height: 50,
+class CategoryList extends StatefulWidget {
+  final List<String> items;
+  final Function(String) callback;
+  CategoryList({@required this.items, @required this.callback});
 
-    child: ListView.builder(
-      itemCount: items.length,
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        return CategoriesTile(title: items[index], callback: callback);
-      },
-    ),
-  );
+  @override 
+  _CategoryListState createState() => _CategoryListState();
 }
 
-class CategoriesTile extends StatelessWidget {
+class _CategoryListState extends State<CategoryList> {
+  int currentSelectedIndex;
+
+  @override 
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 15),
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      height: 33,
+
+      child: ListView.builder(
+        itemCount: widget.items.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return CategoriesTile(
+            index: index,
+            title: widget.items[index],
+            isSelected: currentSelectedIndex == index, 
+            onSelect: () {
+              setState(() {
+                currentSelectedIndex = index;
+              });
+            },
+            callback: widget.callback,
+          );
+        },        
+      ),
+    );
+  }
+}
+
+class CategoriesTile extends StatefulWidget {
+  final int index;
   final String title;
+  final bool isSelected;
+  final VoidCallback onSelect;
   final Function(String) callback;
 
-  CategoriesTile({@required this.title, @required this.callback});
+  CategoriesTile({
+    @required this.index, 
+    @required this.title, 
+    @required this.isSelected,
+    @required this.onSelect,
+    @required this.callback,
+  });
+  
+  @override 
+  _CategoriesTileState createState() => _CategoriesTileState();
+}
 
+class _CategoriesTileState extends State<CategoriesTile> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        this.callback(this.title);
+        widget.onSelect();
+        widget.callback(widget.title);
       },
-
-      child: Container(
+      
+      child: widget.isSelected ? 
+      Container(
         margin: EdgeInsets.only(right: 30),
         alignment: Alignment.center,
+        
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Color(0xff323232), width: 3.0),
+          ),
+        ),
        
         child: Text(
-          title,
+          widget.title,
           style: TextStyle(
             fontSize: 18,
-            fontWeight: FontWeight.w500,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff323232),
+          ),
+        ),
+      ) 
+      :
+      Container(
+        margin: EdgeInsets.only(right: 30),
+        alignment: Alignment.center,
+
+        child: Text(
+          widget.title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
             color: Color(0xff808080),
           ),
         ),
