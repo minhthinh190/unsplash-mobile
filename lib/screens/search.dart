4b9da279ed85c3d90c4a517da88613afcc88dd73
rossoneriNN <17021312@vnu.edu.vn>
2021-04-01 12:03:27 +0700

    update search screen

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 import 'package:unsplash_mobile/widgets/appbar.dart';
 import 'package:unsplash_mobile/widgets/bottom_bar.dart';
 import 'package:unsplash_mobile/widgets/search_field.dart';
+import 'package:unsplash_mobile/widgets/trending_list.dart';
+import 'package:unsplash_mobile/widgets/recommended_list.dart';
 
 import 'package:unsplash_mobile/data/api.dart';
 import 'package:unsplash_mobile/model/photo.dart';
-import 'package:unsplash_mobile/widgets/photo_list.dart';
+import 'package:unsplash_mobile/screens/search_result.dart';
 
 class Search extends StatefulWidget {
   final String searchQuery;
@@ -20,8 +23,27 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   List<Photo> photos = new List();
+  List<Photo> trendingPhotos = new List();
 
   TextEditingController searchController = new TextEditingController();
+
+  getTrendingPhotos() async {
+    final response = await http.get(
+      "https://api.pexels.com/v1/curated?page=30&per_page=5",
+      headers: {"Authorization": apiKey},
+    );
+
+    List<Photo> photos = new List();
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      Photo photo = new Photo();
+      photo = Photo.fromMap(element);
+      photos.add(photo);
+    });
+
+    setState(() { trendingPhotos = photos; });
+  }
 
   getSearchedPhotos(String query) async {
     final response = await http.get(
@@ -30,39 +52,141 @@ class _SearchState extends State<Search> {
     );
 
     photos.clear();
-    
+    List<Photo> newPhotos = new List();
+
     Map<String, dynamic> jsonData = jsonDecode(response.body);
     jsonData["photos"].forEach((element) {
       Photo photo = new Photo();
       photo = Photo.fromMap(element);
-      photos.add(photo);
+      newPhotos.add(photo);
     });
 
-    setState(() {});
+    setState(() { photos = newPhotos; });
+    displayResultScreen(query);
+  }
+
+  displayResultScreen(String query) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SearchResult(query: query, photos: photos)
+      )
+    );
   }
 
   @override 
   void initState() {
     super.initState();
-    getSearchedPhotos(widget.searchQuery);
-    searchController.text = widget.searchQuery;
+    getTrendingPhotos();
   }
 
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              SearchField(controller: searchController, callback: getSearchedPhotos),
-              photoList(photos: photos, context: context),
-            ],
-          ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SearchField(controller: searchController, callback: getSearchedPhotos),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 30),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Trending searches',
+                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    )
+                  ),
+                  trendingList(photos: trendingPhotos),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 35),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 30),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Trending topics',
+                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    )
+                  ),
+                  categoryList(items: ['People', 'Architecture', 'Space', 'Current Events'], callback: getSearchedPhotos),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15),
+              child: Column(  
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 30),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Recommended for you',
+                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    )
+                  ),
+                  RecommendedList(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: BottomBar(selectedIndex: 1),
     );
   }
+}
+
+Widget categoryList({List<String> items, Function(String) callback, context}) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+
+    child: Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            trendingTopic(title: items[0], callback: callback),
+            trendingTopic(title: items[1], callback: callback),
+          ],
+        ),
+        SizedBox(height: 15),
+        Row(
+          children: <Widget>[
+            trendingTopic(title: items[2], callback: callback),
+            trendingTopic(title: items[3], callback: callback),
+          ],
+        ),
+      ],
+    )
+  );
+}
+
+Widget trendingTopic({String title, Function(String) callback, context}) {
+  return GestureDetector(
+    onTap: () {
+      callback(title);
+    },
+    
+    child: Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 13),
+      alignment: Alignment.center,
+
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: Color(0xffececec),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xff808080)),
+      ),
+    )
+  );
 }
