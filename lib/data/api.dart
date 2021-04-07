ee87625014d4be511387773ffb3a02437fc31831
justmb2 <17021298@vnu.edu.vn>
2021-04-07 23:51:26 +0700

    update image api

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -1,9 +1,3 @@
-// Pexels API
-String pexelsApiKey = '563492ad6f91700001000001f31eeed093dd4731a50782298d8d4715';
-
-String pexelsLatestPhotos = 'https://api.pexels.com/v1/curated?per_page=16';
-
-
 // Unsplash API
 String unsplashApiKey = 'Client-ID dTUBUnPASfrsKOWYzVpAwvkr9Ks2MuY3i5YCvSqAxWQ';
 
