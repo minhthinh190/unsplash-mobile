be86da187de80dc56de41e0eed83b42ac86636e0
rossoneriNN <17021312@vnu.edu.vn>
2021-04-07 23:36:20 +0700

    update trending images widget

--- lib/widgets/search/trending_images.dart
+++ lib/widgets/search/trending_images.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:unsplash_mobile/model/photo.dart';
 
-Widget trendingImages({List<Photo> photos, context}) {
+import 'package:unsplash_mobile/screens/selected_image.dart';
+
+Widget trendingImages({List<UnsplashPhoto> photos, context}) {
   return Container(
     height: 130,
     margin: EdgeInsets.only(top: 20),
@@ -11,14 +13,25 @@ Widget trendingImages({List<Photo> photos, context}) {
       scrollDirection: Axis.horizontal,
 
       itemBuilder: (context, index) {
-        return Container(
-          width: 105,
-          margin: index == 0 ? 
-            EdgeInsets.only(left: 30, right: 15) : EdgeInsets.only(right: 15),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.network(photos[index].src.portrait, fit: BoxFit.cover),
-          ),
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) => SelectedImage(
+                author: photos[index].author,
+                imageSrc: photos[index].source.regular,
+              ))
+            );
+          },
+
+          child: Container(
+            width: 105,
+            margin: index == 0 ? 
+              EdgeInsets.only(left: 30, right: 15) : EdgeInsets.only(right: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(photos[index].source.small, fit: BoxFit.cover),
+            ),
+          )
         );
       },
     ),
