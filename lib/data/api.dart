6956e2242b614fe7dd20956dd9c2482beaec6ef5
justmb2 <17021298@vnu.edu.vn>
2021-04-07 23:40:59 +0700

    modify image api

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -1,3 +1,12 @@
-String newPhotos = "https://api.pexels.com/v1/curated?per_page=16";
+// Pexels API
+String pexelsApiKey = '563492ad6f91700001000001f31eeed093dd4731a50782298d8d4715';
 
-String apiKey = "563492ad6f91700001000001f31eeed093dd4731a50782298d8d4715";
+String pexelsLatestPhotos = 'https://api.pexels.com/v1/curated?per_page=16';
+
+
+// Unsplash API
+String unsplashApiKey = 'Client-ID dTUBUnPASfrsKOWYzVpAwvkr9Ks2MuY3i5YCvSqAxWQ';
+
+String unsplashPhotos = 'https://api.unsplash.com/photos?';
+String unsplashSearch = 'https://api.unsplash.com/search/photos?';
+String unsplashRandom = 'https://api.usnplash.com/photos/random?';
