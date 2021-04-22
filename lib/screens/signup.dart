4477a0bc1f58f1bd094b01f64d8044fa29cf8f1d
justmb2 <17021298@vnu.edu.vn>
2021-04-22 11:57:53 +0700

    create sign up page

---
+++ lib/screens/signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:unsplash_mobile/screens/home.dart';
+/*
+class SignUpPage extends StatefulWidget {
+  @override 
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+*/
