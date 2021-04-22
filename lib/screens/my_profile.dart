6917906411a69f41531eaccfe5b4f2d5fe2f892c
justmb2 <17021298@vnu.edu.vn>
2021-04-22 11:57:21 +0700

    update my profile screen

--- lib/screens/my_profile.dart
+++ lib/screens/my_profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:unsplash_mobile/screens/signin.dart';
 import 'package:unsplash_mobile/widgets/appbar.dart';
