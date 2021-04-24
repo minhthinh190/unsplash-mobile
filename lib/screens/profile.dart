bb11b0fff1e5cb01faa37e20d7b96881a0ef0a78
justmb2 <17021298@vnu.edu.vn>
2021-04-24 11:29:54 +0700

    update profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:unsplash_mobile/widgets/profile/profile_photo_list.dart';
+import 'package:unsplash_mobile/widgets/profile/my_photos.dart';
 import 'package:unsplash_mobile/widgets/appbar.dart';
 import 'package:unsplash_mobile/widgets/bottom_bar.dart';
 
@@ -35,7 +34,7 @@ class _ProfileState extends State<Profile> {
 
             return SingleChildScrollView(
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 30),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
 
                 child: Column(
                   children: <Widget>[
@@ -64,71 +63,18 @@ class _ProfileState extends State<Profile> {
                       ],
                     ),
                     SizedBox(height: 30),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                        children: <Widget>[
-                          Column(
-                            children: <Widget>[
-                              Text('${data['total_photos']}', 
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xff323232))
-                              ),
-                              Text('Pics', 
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xffbababa)),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: <Widget>[
-                              Text('${data['followers_count']}', 
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xff323232))
-                              ),
-                              Text('Followers', 
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xffbababa))
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: <Widget>[
-                              Text('${data['following_count']}', 
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xff323232))
-                              ),
-                              Text('Following', 
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xffbababa))
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-                        children: <Widget>[
-                          GestureDetector(
-                            child: Icon(Icons.apps, color: Color(0xffdcdcdc), size: 32),
-                          ),
-                          GestureDetector(
-                            child: Icon(Icons.favorite, color: Color(0xffdcdcdc), size: 32),
-                          ),
-                          GestureDetector(
-                            child: Icon(Icons.add_to_photos, color: Color(0xffdcdcdc), size: 32),
-                          ),
-                        ],
-                      ),
-                    ),
-                    profilePhotoList(context: context),
+                    myPhotos(context: context),
                   ],
                 ),
               ),
             );
           }
 
-          return Text('Loading');
+          return Center(
+            child: CircularProgressIndicator(
+              valueColor: new AlwaysStoppedAnimation<Color>(Color(0xff323232)),
+            )
+          );
         }
       ),
       bottomNavigationBar: BottomBar(selectedIndex: 3),
