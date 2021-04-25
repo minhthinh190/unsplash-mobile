e223c03b5f6ddffaa45ea53d1dfac655cfcf9eee
minhthinh190 <dadzaikitano@gmail.com>
2021-04-26 00:04:21 +0700

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'package:unsplash_mobile/screens/signin.dart';
+import 'package:unsplash_mobile/screens/sub_screen/signin.dart';
 
 void main() => runApp(MyApp());
 
