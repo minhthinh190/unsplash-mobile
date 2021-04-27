b5fa4bff8c39f9135272597ce93cdb3b6512361c
rossoneriNN <17021312@vnu.edu.vn>
2021-04-27 18:12:29 +0700

    create signin screen test

--- test/signinpage_test.dart
+++ test/signinpage_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:unsplash_mobile/screens/signin.dart';
+import 'package:unsplash_mobile/screens/sub_screen/signin.dart';
 
 void main() {
   // Test case 1
