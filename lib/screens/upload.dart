002b01a6939b4f2899079d989726d03f11b1f1db
justmb2 <17021298@vnu.edu.vn>
2021-04-28 15:14:42 +0700

    update upload screen

--- lib/screens/upload.dart
+++ lib/screens/upload.dart
@@ -1,49 +1,17 @@
 import 'package:flutter/material.dart';
 
-import 'package:unsplash_mobile/screens/signin.dart';
 import 'package:unsplash_mobile/widgets/appbar.dart';
 import 'package:unsplash_mobile/widgets/bottom_bar.dart';
-
-import 'package:firebase_auth/firebase_auth.dart';
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class Upload extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+
       appBar: MyAppBar(),
       body: Center(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
-              ),
-            ),
-            backgroundColor: MaterialStateProperty.all<Color>(Color(0xff323232)),
-              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                EdgeInsets.symmetric(horizontal: 60, vertical: 10),
-              ),
-          ),
-          
-          child: Text(
-            'Sign Out',
-            style: TextStyle(fontSize: 20),
-          ),
-
-          onPressed: () async { 
-            final User user = _auth.currentUser;
-            await _auth.signOut();
-
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('${user.email} signed out.'))
-            );
-            Navigator.pushReplacement(context, MaterialPageRoute(
-              builder: (context) => SignInPage(),
-            ));
-          },
-        ),
+        child: Text('')
       ),
       bottomNavigationBar: BottomBar(selectedIndex: 2),
     );
